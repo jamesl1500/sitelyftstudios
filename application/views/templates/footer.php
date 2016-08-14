f35efcc8f926a82da6ad0b92b39b0de8c84daaac
James Latten JR <codetastic2@gmail.com>
2016-08-14 09:59:26 -0400

    tracking

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -13,5 +13,15 @@
 <?php if(isset($javascript) && $javascript != ""){ ?>
     <script src="<?php echo site_url('assets/js/pages/'.$javascript.'.js'); ?>" type="text/javascript"></script>
 <?php } ?>
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-82435788-1', 'auto');
+    ga('send', 'pageview');
+
+</script>
 </body>
 </html>
