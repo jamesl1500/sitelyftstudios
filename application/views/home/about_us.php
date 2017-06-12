2294b1b6d735a9e284bad4e7031a6987c96e9b2f
James Latten JR <james@sitelyftstudios.com>
2017-06-11 22:06:04 -0400

    About us edits
    These are some edits with the text for the about us page

--- application/views/home/about_us.php
+++ application/views/home/about_us.php
@@ -7,11 +7,46 @@
         </div>
     </div>
 </div>
-<div class="mainContainer container">
+<div class="mainContainer container v">
     <div class="topLogo">
         <center><img src="<?php echo base_url(); ?>assets/images/icons/sitelyft-circle-logo.png"></center>
     </div>
     <div class="middleDesk">
-        <p>Here at Sitelyft we specialize in Front-end and Back-end website development. We were founded in 2016 with a commitment to making quality websites and code. We also can do weekly or monthly maintenance on your existing websites too! We also can help get your website out there in the world with our amazing SEO Optimization. Also we can take your PSD, InDesign, or even your Illustrator files and turn them into beautiful code.</p>
+        <p style="font-size: 1.3em;text-align: left;font-weight: 400;padding: 30px;padding-top: 0px;">Here at Sitelyft we specialize in Front-end and Back-end website development. We were founded in 2017 with a commitment and passion to making quality websites and code. We're very honest and confident, we will only give you the best! Though we may be small, we're dedicated to providing the best in Web Development and leave you with a smile after you see the finished product.</p>
+        <div class="b">
+            <h3>Communication</h3>
+            <p>We have impressive communication skills when it comes to our clients. We use <b>WhatsApp, Skype, Email, Phone, or Viber</b> to communicate with our clients. We're very active on our communication platforms so rest assure that you'll be able to contact us whenever you need us! We always keep in contact with you to update you on the progress of the project!</p>
+        </div>
+        <div class="b">
+            <h3>Project Delivery</h3>
+            <p>
+                An initial payment is required to start work on your project. After the first initial invoice is paid, we will go full speed on your project and have things delivered quickly. Here is how the system works:
+            <ul>
+                <li>1. Sitelyft Studios will invoice you the initial estimate w/ tax</li>
+                <li>2. For work to start, you will need to pay 3/4 of the initial invoice</li>
+                <li>3. After work is completed and you're satisfied, you will need to pay the last 1/4 of the invoice</li>
+                <li>4. Lastly we will hand over full ownership over the project after the last bit of the invoice is paid</li>
+            </ul>
+            </p>
+        </div>
+        <div class="b">
+            <h3>Payments</h3>
+            <p>
+                At Sitelyft, we accept a wide range of payment options. We accept <b>Credit/Debit cards, Paypal, Chase Quickpay, cash and checks</b> as well! We also charge tax to all of our invoices. With <b>late payments</b> we charge an extra $35 every day the invoice is overdue. With <b>early payments</b> we will deduct $35 from the invoice if you pay within 24 hours of receiving the invoice!
+            </p>
+        </div>
+        <div class="b">
+            <h3>Payment Terms</h3>
+            <p>
+                An initial payment is required to start work on your project. After the first initial invoice is paid, we will go full speed on your project and have things delivered quickly. Here is how the system works:
+                <ul>
+                    <li>1. Sitelyft Studios will invoice you the initial estimate w/ tax</li>
+                    <li>2. For work to start, you will need to pay 3/4 of the initial invoice</li>
+                    <li>3. After work is completed and you're satisfied, you will need to pay the last 1/4 of the invoice</li>
+                    <li>4. Lastly we will hand over full ownership over the project after the last bit of the invoice is paid</li>
+                </ul>
+            </p>
+        </div>
+
     </div>
 </div>
